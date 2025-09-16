16cdd6d026ab601cbd45d13594bee935e92d7fc2
Magali Oliveira <magali@MBP-de-Magali.lan>
2025-09-16 20:07:28 +0100

    added new font text and  new element style on home

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./style/index.scss"
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -10,23 +10,23 @@ function Contacts() {
             <ul>
                 <li>
                     <a href="mailto:contact.magalioliveira@gmail.com?subject=Contact%20from%20site%20&body=Hi,%20Magali!%0A%0A%0AI%20visited%20your%20portfolio,%20... ">
-                        <BsFillEnvelopeFill />
+                        <BsFillEnvelopeFill size={50} />
                     </a>
                 </li>
                 <li>
                     <a href="https://www.linkedin.com/in/magalideoliveira" target="_blank" rel="noopener noreferrer">
-                        <BsLinkedin />
+                        <BsLinkedin size={50} />
                     </a>
                 </li>
                 <li>
                     <a href="https://wa.me/351938306590?text=Hello%20Magali,%0A%0AI%20just%20visited%20your%20portfolio%20and%20would%20love%20to%20discuss%20future%20projects%20with%20you!"
                         target="_blank" rel="noopener noreferrer">
-                        <BsWhatsapp />
+                        <BsWhatsapp size={50} />
                     </a>
                 </li>
                 <li>
                     <a href="https://github.com/magaliolvr" target="_blank" rel="noopener noreferrer">
-                        <BsGithub />
+                        <BsGithub size={50} />
                     </a>
                 </li>
             </ul>
